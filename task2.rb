b5ba36efffaa582fcb1c5e97f0929508092373a8
Vadim Petrov <pvg.tag@yandex.ru>
2018-06-03 21:02:51 +0300

    task2 к тестированию

--- task2.rb
+++ task2.rb
@@ -3,12 +3,19 @@
 # Put your code here
 s = ARGV[0]
 s1 = s[1, s.length]
-puts s.length
+#puts s.length
 i = 0
 while i <= s.length
   s2 = s[0, i]
-  puts s2
+  #puts s2
   i = i + 1
-  puts i
+  #puts i
+  #puts s1.include?(s2)
+  if (not s1.include?(s2))
+    s2 = s2[0..-2]
+    #puts s2
+    break
+  end
 end
-puts s1
+#puts s1
+puts s2
