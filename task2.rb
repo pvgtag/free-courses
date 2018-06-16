558ce0a908619cc179c1baf33c3b0a007f482171
Vadim Petrov <pvg.tag@yandex.ru>
2018-06-16 16:02:38 +0300

    добавлена проверка на пустую строку

--- task2.rb
+++ task2.rb
@@ -1,27 +1,31 @@
 # Task 2
 
 # Put your code here
-s = ARGV[0].chomp
+s = ARGV[0]
 i = 0;
 s3 = '';
-while (i < s.size - 1)
-  j = 0;
-  is_double = false
-  while (j < s.size - i)
-    s1 = s[j,i];
-    # puts s1;
-    idx1 = s.index(s1);
-    # puts s.rindex(s1);
-    if (s.index(s1) != s.rindex(s1))
-      s3 = s1
-      is_double = true
+if s != nil
+  s = s.chomp
+  while (i < s.size - 1)
+    j = 0;
+    is_double = false
+    while (j < s.size - i)
+      s1 = s[j,i];
+      # puts s1;
+      idx1 = s.index(s1);
+      # puts s.rindex(s1);
+      if (s.index(s1) != s.rindex(s1))
+        s3 = s1
+        is_double = true
+      end;
+      j += 1;
     end;
-    j += 1;
-  end;
-  if !is_double then
-    break;
-  end;
-  i += 1;
+    if !is_double then
+      break;
+    end;
+    i += 1;
+  end
 end
 puts s3;
+
 
